a29d345167c5a820c318c46e9f9c5b1620a80759
ColineVL <coline@vleeuwen.fr>
2020-05-03 13:34:06 +0200

    Correction de newtask
    On ne peut plus mettre de priorités négatives.
    Quand on fait une erreur dans le formulaire newtask, les valeurs entrées avant sont conservées (assignee, start_date, due_date)

--- projet/taskmanager/forms.py
+++ projet/taskmanager/forms.py
@@ -9,6 +9,13 @@ class TaskForm(forms.ModelForm):
         exclude = ('project',)
         # Quand l'utilisateur crée une tache ou la modifie, il ne doit pas changer le projet associé.
 
+    def clean_priority(self):
+        priority = self.cleaned_data['priority']
+        if priority < 0:
+            raise forms.ValidationError("La priorité doit être un entier positif !")
+        return priority
+    # L'utilisateur ne pourra pas entrer de priorité négative
+
 
 # Form pour entrer une nouvelle information complémentaire dans un journal
 class NewEntryForm(forms.Form):
