344c0a180efe7fe1987a67481f1ee449ed07ce2b
ColineVL <coline@vleeuwen.fr>
2020-05-10 17:40:38 +0200

    Spiderweb graphe placé
    Dans membersbyproject

---
+++ projet/taskmanager/static/js/spiderweb.js
@@ -0,0 +1,66 @@
+Highcharts.chart('spiderweb', {
+
+    chart: {
+        polar: true,
+        type: 'line'
+    },
+
+    title: {
+        align: "center",
+        text: 'Nombre de tâches par personne',
+    },
+
+    // pane: {
+    //     size: '80%'
+    // },
+
+    xAxis: {
+        categories: ['Sales', 'Marketing', 'Development', 'Customer Support',
+            'Information Technology', 'Administration'],
+        tickmarkPlacement: 'on',
+        lineWidth: 0
+    },
+
+    yAxis: {
+        gridLineInterpolation: 'polygon',
+        lineWidth: 0,
+        min: 0
+    },
+
+    tooltip: {
+        shared: true,
+        pointFormat: '<span style="color:{series.color}">{series.name}: <b>${point.y:,.0f}</b><br/>'
+    },
+
+    legend: {
+        enabled: false,
+        align: 'right',
+        verticalAlign: 'middle',
+        layout: 'vertical'
+    },
+
+    series: [{
+        name: 'Allocated Budget',
+        data: [43000, 19000, 60000, 35000, 17000, 10000],
+        pointPlacement: 'on'
+    }],
+
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    align: 'center',
+                    verticalAlign: 'bottom',
+                    layout: 'horizontal'
+                },
+                pane: {
+                    size: '70%'
+                }
+            }
+        }]
+    }
+
+});
