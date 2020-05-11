2099feb3f0ba03d40e0452d27c2340b7c694ebe8
Nicola Imperatore <nicolaimpe@gmail.com>
2020-05-11 12:12:54 +0200

    TestCommit

--- projet/taskmanager/filters.py
+++ projet/taskmanager/filters.py
@@ -42,7 +42,7 @@ class TaskFilter(django_filters.FilterSet):
                                                  field_class=forms.IntegerField,
                                                  label='progrès',
                                                  )
-
+    #TESTTEST
     # class Meta:
     #   model = Task
     # fields = ['name']
