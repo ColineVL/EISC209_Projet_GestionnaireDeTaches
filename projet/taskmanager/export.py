3d98a36cf94825aee3051fc627a8ef9302b08db5
Alexis Vandewalle <a.vandewalle@isae.fr>
2020-05-11 14:22:56 +0200

    ajout de commentaires dans export

--- projet/taskmanager/export.py
+++ projet/taskmanager/export.py
@@ -12,7 +12,7 @@ def create_file(file_type, filename, queryset, fields, zipObj):
     :param file_type: the type of the file to generate ('csv','xls','json','html' or 'yaml')
     :param filename: the name of the file to generate
     :param queryset: the table contained in the file
-    :param resource: the resource assiated withe the queryset
+    :param resource: the resource assiated with the queryset
     :param zipObj: the zip object in wich we want to write
     :return: nothing
     """
@@ -33,12 +33,20 @@ def create_file(file_type, filename, queryset, fields, zipObj):
     os.remove(filename)
 
 def queryset_to_csv(queryset, fields, filename):
-    file = open(filename,'w')
-    writer = csv.writer(file)
-    writer.writerow([field for field in fields])
+    """
+    This function create a csv file with a given queryset
+    :param queryset: the queryset that we want to export in a file
+    :param fields: the fields that we want in our table (it must be the same names as those in the models
+    :param filename: the name of the future file
+    :return: nothing
+    """
+    file = open(filename,'w') # we create an empty file
+    writer = csv.writer(file) #we create a writer linked to the file
+    writer.writerow([field for field in fields]) # we write the fields
     for row in queryset:
         line = []
         for field in fields:
+            #if the field is a ManyToManyField, we must save a list, in this case a list of username
             if field == 'members':
                 line.append([member.username for member in row.__getattribute__(field).all()])
             else:
@@ -47,8 +55,15 @@ def queryset_to_csv(queryset, fields, filename):
     file.close()
 
 def queryset_to_json(queryset, fields, filename):
+    """
+    This function does the same thing that queryset_to_csv but for json file
+    :param queryset:
+    :param fields:
+    :param filename:
+    :return:
+    """
     file = open(filename,"w")
-    result = dict()
+    result = dict() #we create a dictionnary that contains dictionnaries for each items
     for row in queryset:
         row_dict = dict()
         for field in fields:
@@ -57,10 +72,17 @@ def queryset_to_json(queryset, fields, filename):
             else:
                 row_dict[field] = row.__getattribute__(field).__str__()
         result[row.id] = row_dict
-    file.write(json.dumps(result))
+    file.write(json.dumps(result)) # we save this dictionnary
     file.close()
 
 def queryset_to_xml(queryset, fields, filename):
+    """
+    This function does the same thing that queryset_to_csv but for xml/html file
+    :param queryset:
+    :param fields:
+    :param filename:
+    :return:
+    """
     file = open(filename,"wb")
     table = ET.Element('table')
     head = ET.SubElement(table,'tr')
@@ -81,13 +103,20 @@ def queryset_to_xml(queryset, fields, filename):
     file.close()
 
 def queryset_to_xlsx(queryset, fields, filename):
-    file = xlsx.Workbook(filename)
-    worksheet = file.add_worksheet()
-    bold = file.add_format({'bold':True})
+    """
+    This function does the same thing that queryset_to_csv but for xlsx file
+    :param queryset:
+    :param fields:
+    :param filename:
+    :return:
+    """
+    file = xlsx.Workbook(filename) # we create an xlsx file
+    worksheet = file.add_worksheet() # we create a sheet inside this file
+    bold = file.add_format({'bold':True}) # we create a bold format for the header
     count=0
     alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     for field in fields:
-        worksheet.write(alphabet[count]+(1).__str__(), field, bold)
+        worksheet.write(alphabet[count]+(1).__str__(), field, bold) # we write a field in a cell with bold format
         count +=1
     row_number=2
     for row in queryset:
@@ -103,8 +132,15 @@ def queryset_to_xlsx(queryset, fields, filename):
     file.close()
 
 def queryset_to_yaml(queryset, fields, filename):
+    """
+    This function does the same thing that queryset_to_csv but for yaml file
+    :param queryset:
+    :param fields:
+    :param filename:
+    :return:
+    """
     file = open(filename, "w")
-    result = []
+    result = [] # we create an empty list
     for row in queryset:
         row_dict = dict()
         for field in fields:
@@ -112,6 +148,6 @@ def queryset_to_yaml(queryset, fields, filename):
                 row_dict[field] = [member.username for member in row.__getattribute__(field).all()]
             else:
                 row_dict[field] = row.__getattribute__(field).__str__()
-        result.append(row_dict)
+        result.append(row_dict) # we add a dictionnary to this list for each rows
     file.write(yaml.dump(result))
     file.close()
